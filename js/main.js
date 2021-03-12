470075e93d8d65b75ee5719286a0f5ecd18cbc49
quangdev98 <dvq.dev@gmail.com>
2021-03-12 17:47:20 +0700

    feature/new-index-page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-	let heightDevice = $(window).height();
-	let heightHeader= $('#header').outerHeight();
-	let heightBody= $('#wrap-main').outerHeight();
-	let heightFooter= $('#footer').outerHeight();
-	console.log(heightBody);
-	
+
+	let footer = document.getElementById('footer');
+	function setFooter(){
+		let heightDevice = $(window).height();
+		let heightHeader= $('#header').outerHeight();
+		let heightBody= $('#wrap-main').outerHeight();
+		let heightFooter= $('#footer').outerHeight();
+		// console.log(heightBody);
+		let sumBody = heightHeader + heightBody + heightFooter;
+		if (sumBody < heightDevice) {
+			$('footer').removeClass('position');
+			$('footer').addClass('position');
+		}else{
+			$('footer').removeClass('position');
+		}
+		// console.log(sumBody);
+		// console.log(heightDevice);
+	}
+	setFooter();
+	$(window).resize(function(){
+		setFooter();
+	})
  })
